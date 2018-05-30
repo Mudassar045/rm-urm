4510de1f8dc49faf3a01895c4eea378769ec7a85
Mudassar045 <Mudassaralics007@gmail.com>
2018-05-30 08:56:53 +0000

    code formatted

--- src/removeFile.c
+++ src/removeFile.c
@@ -7,29 +7,30 @@
 #include <sys/types.h>
 #define MAX_BYTES 1024
 #define MAX_CHARS 1024
-char* concat(const char*, const char*);
-int removeFile(char* filename)
+char *concat(const char *, const char *);
+int removeFile(char *filename)
 {
   struct stat mystat;
   char buff[MAX_CHARS];
-  int rv = stat(filename,&mystat);
+  int rv = stat(filename, &mystat);
   int mode_t = mystat.st_mode;
 
-  int fdf = open(filename,O_RDONLY);
-  char *newfile = concat(".mdrm_temp/",filename);
-  int fds = open(newfile,O_TRUNC|O_CREAT|O_RDWR,mode_t);
-  if(fds==-1)
+  int fdf = open(filename, O_RDONLY);
+  char *newfile = concat(".mdrm_temp/", filename);
+  int fds = open(newfile, O_TRUNC | O_CREAT | O_RDWR, mode_t);
+  if (fds == -1)
   {
-    fprintf(stderr,"rm: unable to remove file");
+    fprintf(stderr, "rm: unable to remove file");
     exit(0);
-  }else
+  }
+  else
   {
-    while(1)
+    while (1)
     {
-    int byte_t = read(fdf,buff,MAX_BYTES);
-     if (byte_t<=0)    
+      int byte_t = read(fdf, buff, MAX_BYTES);
+      if (byte_t <= 0)
       {
-         break; // file reading complete
+        break; // file reading complete
       }
       write(fds, buff, byte_t);
     }
@@ -37,17 +38,19 @@ int removeFile(char* filename)
   close(fdf); // file descriptor for original file
   close(fds); // file descriptor for trash file
   rv = remove(filename);
-  if(rv==-1)
+  if (rv == -1)
   {
-    fprintf(stderr,"rm: unable to perform remove operation");
+    fprintf(stderr, "rm: unable to perform remove operation");
     return -1;
-  }else return 0;
+  }
+  else
+    return 0;
 }
-char* concat(const char *s1, const char *s2)
+char *concat(const char *s1, const char *s2)
 {
-    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the null-terminator
-    //in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
+  char *result = malloc(strlen(s1) + strlen(s2) + 1); //+1 for the null-terminator
+  //in real code you would check for errors in malloc here
+  strcpy(result, s1);
+  strcat(result, s2);
+  return result;
 }
